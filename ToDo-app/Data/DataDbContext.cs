32502ad0a2854289e8da9373314a41cc2f602ec4
caiobraga98 <kaio.braga98@gmail.com>
2025-11-02 20:54:17 -0300

    Refactor project structure and add DataDbContext
    Removed `<Folder Include="Data\" />` from `ToDo-app.csproj`, indicating a shift in how the `Data` folder is managed.
    Added `DataDbContext` class in `DataDbContext.cs` under `ToDo_app.Data` namespace.
    - Implements `DbContext` for database interaction.
    - Includes `Tarefas` property as `DbSet<Model.Tarefa>`.
    - Supports Entity Framework Core with `DbContextOptions`.

---
+++ ToDo-app/Data/DataDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDo_app.Data
+{
+    public class DataDbContext: DbContext
+    {
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Model.Tarefa> Tarefas { get; set; }
+    }
+}
