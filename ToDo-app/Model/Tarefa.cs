f4a71ff2bf185cea4eb64f83dcf269a34a87c436
caiobraga98 <kaio.braga98@gmail.com>
2025-11-02 20:46:42 -0300

    Add Tarefa class and update project structure
    The `<Folder Include="Model\" />` entry was removed from the
    `ToDo-app.csproj` file, simplifying the project structure.
    
    Added a new `Tarefa` class to the `ToDo_app.Model` namespace
    in `Tarefa.cs`. This class represents a task entity with
    properties for `Id`, `titulo`, `descricao`, `dataCriacao`,
    `dataConclusao`, and `concluida`.

---
+++ ToDo-app/Model/Tarefa.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDo_app.Model
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public required string titulo { get; set; }
+        public string? descricao { get; set; }
+        public DateTime dataCriacao { get; set; }
+        public DateTime? dataConclusao { get; set; }
+        public bool concluida { get; set; }
+    }
+}
