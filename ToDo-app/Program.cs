53841a9ce33e9d581c4524d7e4cfd848b91f6868
caiobraga98 <kaio.braga98@gmail.com>
2025-11-04 17:59:13 -0300

    Service to be injected.

--- ToDo-app/Program.cs
+++ ToDo-app/Program.cs
@@ -1,20 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using ToDo_app.Data;
+using ToDo_app.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.  
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle  
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DataDbContext")));
+   options.UseSqlite(builder.Configuration.GetConnectionString("DataDbContext")));
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.  
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
