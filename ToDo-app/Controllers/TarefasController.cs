aa60a9a08c1ee70d8716aa31005bebda96d17902
caiobraga98 <kaio.braga98@gmail.com>
2025-11-05 17:37:53 -0300

    Creation of controller and injecting dependency

---
+++ ToDo-app/Controllers/TarefasController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDo_app.Model;
+using ToDo_app.Repository;
+
+namespace ToDo_app.Controllers
+{
+    // Adicionar os atributos e herança
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TarefasController : ControllerBase 
+    {
+        private readonly IRepository<Tarefa> _repository;
+        public TarefasController(IRepository<Tarefa> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var tarefas = await _repository.GetAllAsync();
+            return Ok(tarefas);
+        }
+
+    }
+}
